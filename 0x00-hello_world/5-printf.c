723992e0862b90239880bf606a28446bf1320552
Edwin Kahugu <edwinmaregwa@gmail.com>
2023-03-16 08:01:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - controls the program
+ *
+ * Return: gives 0 upon successful completion
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
