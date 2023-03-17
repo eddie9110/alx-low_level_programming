ff29c5a681c0bcf3312745f664473772be6eab30
Edwin Kahugu <edwinmaregwa@gmail.com>
2023-03-17 12:27:24 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - the main function
  *
- * Return: always 0
+ * Return: always 0 
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
+
