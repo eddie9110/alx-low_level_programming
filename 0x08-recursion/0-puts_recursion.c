dabbd9dae4e1ec63e895a07d59f81b246e3ee0d5
eddie9110 <edwinmaregwa@gmail.com>
2023-04-05 04:45:48 -0700

    function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this function outputs a string
+ * @char *s: this is the parameter of interest in the ftn mentioned above
+ * return: no return because of void data type
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s)
+	{
+		_putchar(*s)
+		_puts_recursion
+	}
+}	
