9ff53cf0621d7ac13a1bbd2246f58fc038973868
eddie9110 <edwinmaregwa@gmail.com>
2023-04-09 23:36:30 -0700

    program that prints its name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main ftn 
+ * @argc: parameter for the no. of argument passed in terminal
+ * argv: parameter for displaying the args passed in the terminal
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
