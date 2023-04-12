1216984734f45dd35433db3572405244ab148ab5
eddie9110 <edwinmaregwa@gmail.com>
2023-04-11 22:44:36 -0700

    function that creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- *create_array – ftn will create an array of size and assign char c
- *@size: parameter size will assign size
- *@c: parameter char to assign a character
- *Return: str  if successful and null if ftn fails
+ * create_array - create_array creates an array of size and assign char c
+ * @size: parameter size will assign size
+ * @c: parameter char to assign a character
+ * Return: str  if successful and null if ftn fails
  */
 
 char *create_array(unsigned int size, char c)
@@ -14,10 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int a;
 
 	ptr = malloc(sizeof(char) * size);
-	if (size == 0 || ptr == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
 
 	for (a = 0; a < size; a++)
-		ptr[a] = c;
-	return (ptr);
+		str[a] = c;
+	return (str);
 }
+
