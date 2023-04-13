bbe58ca45c802fca43475cdcc88f34b4bcadb5be
eddie9110 <edwinmaregwa@gmail.com>
2023-04-13 02:28:00 -0700

    function that concatenates two strings

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - custom malloc ftn
+ * @b: the amount of bytes assigned
+ * Return: a void pointer == str
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+
+	if (str == NULL)
+	{
+		exit (98);
+	}
+	return (str);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - ftn to connect two strings
+ * @s2: parameter represents string to be concatenated to s1
+ * @S2: parameter represents string to be concatenated to s2
+ *Return: z
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *i;
+	unsigned int a = 0, b = 0; 
+	unsigned int y, z = 0;
+
+	while (s1 && s1[y])
+		y++;
+	while (s2 && s2[z])
+		z++;
+
+	if (n < z)
+		i = malloc(sizeof(char) * (y + n + 1));
+	else
+		i = malloc(sizeof(char) * (y + z + 1));
+
+	if (!i)
+		return (NULL);
+
+	while (a < y)
+	{
+		i[a] = s1[a];
+		a++;
+	}
+
+	while (n < z && a < (y + n))
+		i[a++] = s2[b++];
+
+	while (n >= z && a < (y + z))
+		i[a++] = s2[b++];
+
+	i[a] = '\0';
+
+	return (i);
+}
