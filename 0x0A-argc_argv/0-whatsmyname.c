8c2343b829d1ea9fa80b828e12d0f8b4c775e3b2
eddie9110 <edwinmaregwa@gmail.com>
2023-04-13 04:49:10 -0700

    program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 /**
  * main - the main ftn
  * @argc: parameter for the no. of argument passed in terminal
- * argv: parameter for displaying the args passed in the terminal
+ * @argv: parameter for displaying the args passed in the terminal
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	printf("%s\n",*argv);
+	printf("%s\n", *argv);
 	return (0);
 }
