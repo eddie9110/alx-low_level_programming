c6f98149a51c9325ecc1f2afa4f1e5af20b27d0b
eddie9110 <edwinmaregwa@gmail.com>
2023-04-28 03:09:39 -0700

    printing elements of a node in a singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * print_list - ftn that prints contents of a list
+ * @h: a pointer to head of list
+ * Return: op(no. of elements in list nodes)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t op = 0;
+
+
+	if (h)
+	{
+		while (h != NULL)
+		{
+			printf("[%d] %s\n" ,h->len, h->str);
+			h = h->next;
+			op++;
+		}
+	}
+	return (op);
+}
