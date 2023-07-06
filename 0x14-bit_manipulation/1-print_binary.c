4d23d48ae6d0318abe51cd59603985c70d67b9bd
eddie9110 <edwinmaregwa@gmail.com>
2023-07-06 14:31:46 -0700

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - a ftn that outputs a bin of a de no.
+ * @n: no. to convert to binary format
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int ujc = 0;
+	unsigned long int bnry;
+	int cnt = 0;
+
+
+	if (!cnt)
+		_putchar('0');
+
+	for (ujc = 63 ; ujc >= 0 ; ujc--)
+	{
+		bnry = n >> ujc;
+
+
+		if (bnry & 1)
+		{
+			_putchar('1');
+			cnt++;
+		}
+
+		else if (cnt)
+			_putchar('0');
+	}
+
+}
