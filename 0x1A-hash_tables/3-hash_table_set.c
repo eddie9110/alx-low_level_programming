edd7b5ef6a52e6f61870809e9153baff245847ee
eddie9110 <edwinmaregwa@gmail.com>
2023-09-13 08:14:54 -0700

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,19 +19,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	cpy_value = strdup(value);
-
 	if (!cpy_value)
 		return (0);
-
 	idx = key_index((const unsigned char *)key, ht->size);
-
 	if (ht->array[idx] && !strcmp(ht->array[idx]->key, key))
 	{
 		free(ht->array[idx]->value);
 		ht->array[idx]->value = cpy_value;
 		return (1);
 	}
-
 	for (head = ht->array[idx]; head; head = head->next)
 	{
 		if (!strcmp(head->key, key))
@@ -41,20 +37,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 	}
-
 	n_node = malloc(sizeof(hash_node_t));
-
 	if (!n_node)
 		return (0);
-
 	n_node->key = strdup(key);
-
 	if (!n_node->key)
 		return (0);
-
 	n_node->value = cpy_value;
 	n_node->next = ht->array[idx];
 	ht->array[idx] = n_node;
-
 	return (1);
 }
